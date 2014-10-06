26d89c93d11363d0bbe8bfe8868872e3d29af4c3
Mykal Cuin <mykcuin@gmail.com>
2014-10-06 11:28:58 -0600

    Felshed out a basic a class, methods, and variables.

---
+++ ClassesAndObjects/src/Bicycle.java
@@ -0,0 +1,38 @@
+
+public class Bicycle {
+	
+	private int cadence;
+	private int gear;
+	private int speed;
+	
+	public Bicycle(int startCadence, int startSpeed, int startGear) {
+		gear = startGear;
+		speed = startSpeed;
+		cadence = startCadence;
+	}
+	
+	public int getCadence() {
+		return cadence;
+	}
+	
+	public void setCadence(int newValue) {
+		cadence = newValue;
+	}
+	
+	public int getSpeed() {
+		return speed;
+	}
+	
+	public void setSpeed(int newValue) {
+		speed = newValue;
+	}
+	
+	public int getGear() {
+		return gear;
+	}
+	
+	public void setGear(int newValue) {
+		gear = newValue;
+	}
+
+}
