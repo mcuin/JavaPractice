33ec1e0a623421b3393eb31f816012e72a31fd61
Mykal Cuin <mykcuin@gmail.com>
2014-10-06 11:12:08 -0600

    Finished with statements. Skipped return as its pretty self explanitory.

--- Statements/src/Continue.java
+++ Statements/src/Continue.java
@@ -2,8 +2,40 @@
 public class Continue {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String searchMe = "peter piper picked a " + "peck of pickled peppers"; 
+		int max = searchMe.length();
+		int p = 0;
+		
+		for (int i = 0; i < max; i++) {
+			if (searchMe.charAt(i) != 'p') {
+				continue;
+			}
+			p++;
+		}
+		
+		System.out.println("Found " + p + " p's in the string.");
+		
+		String searchMe2 = "Find a substring in me.";
+		String subString = "sub";
+		boolean foundIt = false;
+		
+		int max2 = searchMe2.length() - subString.length();
+		
+		test:
+			for (int i = 0; i <= max; i++) {
+				int n = subString.length();
+				int j = i;
+				int k = 0;
+				while (n-- != 0) {
+					if (searchMe2.charAt(j++) != subString.charAt(k++)) {
+						continue test;
+					} 
+				}
+				foundIt = true;
+				break test;
+			}
+		
+		System.out.println(foundIt ? "Found it" : "Did not find it");
 	}
 
 }
